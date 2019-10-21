c93d3d122768ba61a6edcf77930bb46845e66cd6
Ruan Matos <ruanmatos96@gmail.com>
2019-10-21 17:08:28 -0300

    Adicionando rota de envio de casdastro mais a componentização do cadastro de login

--- Frontend/compassotec/src/index.js
+++ Frontend/compassotec/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './css/login.css';
 import Login from './componentes/Login';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CadastroLogin from './componentes/CadastroLogin';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Login} />
+            <Route path="/Cadastrar" component={CadastroLogin} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
