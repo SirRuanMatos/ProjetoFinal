957f5beb43cff6b18db26f6df6f5b2f29dacae55
Ruan Matos <ruanmatos96@gmail.com>
2019-10-23 17:06:39 -0300

    Adicionando a tela ensinar 2 parte

--- Frontend/compassotec/src/componentes/GrupoForm.js
+++ Frontend/compassotec/src/componentes/GrupoForm.js
@@ -7,24 +7,28 @@ class GrupoForm extends Component {
         return (
             <div>
                 <div className="form-row">
-                    <div className="form-group col-md-6">
+                    <div className="form-group col-md-5">
                         <label>Topico </label><br />
-                        <input type="text" name={"topico" + id} className="form-control" value={id} />
+                        <input type="text" name={"topico" + id} id={"topico" + id} className="form-control" />
                     </div>
                     <div className="form-group col-md-2">
                         <label>Local</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name={"local" + id} id={"local" + id} ref={(input) => this.local = input} />
                     </div>
                     <div className="form-group col-md-2">
                         <label>Data</label>
-                        <input type="date" className="form-control" />
+                        <input type="date" className="form-control" name={"data" + id} id={"data" + id} ref={(input) => this.data = input} />
                     </div>
                     <div className="form-group col-md-2">
                         <label>Hora</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name={"hora" + id} id={"hora" + id} ref={(input) => this.hora = input} />
+                    </div>
+                    <div className="form-group col-md-1">
+                        <label>Remover</label>
+                        <button type="button" onClick={this.props.clicou}>X</button>
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
--- Frontend/compassotec/src/componentes/teste.js
+++ Frontend/compassotec/src/componentes/teste.js
@@ -4,7 +4,7 @@ import GrupoForm from './GrupoForm';
 class Teste extends Component {
     constructor(props) {
         super(props);
-        this.state = { aulas: [], id: 0 };
+        this.state = { aulas: [{ aId: 1 }], id: 1 };
         this.handleAdcionarAulas = this.handleAdcionarAulas.bind(this);
 
     }
@@ -20,14 +20,102 @@ class Teste extends Component {
         });
     }
 
+    escutadorDeInput = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    remover(id) {
+        let vetAulas = this.state.aulas;
+        console.log("clicou no id:" + id);
+        var cont = 0;
+
+        var idElem = 0;
+        vetAulas.map(a => {
+            if (a.aId == id) {
+                idElem = cont;
+            }
+            cont++;
+        });
+        console.log(idElem);
+        console.log(vetAulas);
+        vetAulas.splice(idElem, 1);
+        console.log(vetAulas);
+        // this.setState(aulas.splice(id));
+        this.setState({ aulas: vetAulas });
+    }
+
+
+    envia(event) {
+        event.preventDefault();
+
+        var jsonEnvia = [];
+        this.state.aulas.map(a => {
+            let topico = document.querySelector('#topico' + a.aId).value;
+            let local = document.querySelector('#local' + a.aId).value;
+            let data = document.querySelector('#data' + a.aId).value;
+            let hora = document.querySelector('#hora' + a.aId).value;
+
+            var grupoFormJson = { topico: topico, local: local, data: data, hora: hora }
+
+
+            jsonEnvia.push(grupoFormJson);
+        });
+
+        console.log(JSON.stringify(jsonEnvia));
+
+
+        /* 
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                var texto = document.querySelector('#login').value;
+        
+                if (re.test(texto)) {
+                    const requestInfo = {
+                        method: 'POST',
+                        body: JSON.stringify({ email: this.login.value, senha: this.senha.value }),
+                        headers: new Headers({
+                            'Content-type': 'application/json'
+                        })
+                    };
+        
+                    fetch('http://localhost:8080/login', requestInfo)
+                        .then(response => {
+                            if (response.ok) {
+                                return response.text();
+                            } else {
+                                throw new Error('Não foi possí­vel fazer o login');
+                            }
+                        })
+                        .then(token => {
+                            localStorage.setItem('auth-token', token);
+                            this.props.history.push("/teste");
+                        })
+                        .catch(error => {
+                            this.setState({ msg: error.message });
+                        });
+                }
+                else {
+                    this.setState({ msg: "Digite um email válido" });
+                    //throw new Error("Digite um email válido");
+                }
+         */
+
+    }
+
     render() {
-        const aulas = this.state.aulas.map(a => <GrupoForm id={a} />)
+
+        const aulas = this.state.aulas.map(a => <GrupoForm key={a.aId} id={a} clicou={this.remover.bind(this, a.aId)} />
+        )
 
         return (
             <div className="container">
-                <form>
+                <form onSubmit={this.envia.bind(this)}>
                     {aulas}
                     <a onClick={this.handleAdcionarAulas}>Adicionar</a>
+                    <button type="submit">Enviar</button>
                 </form>
             </div>
         );
